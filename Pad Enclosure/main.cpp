34392139bdaed0796022c8971c90b1fcaf8bf54e
Matthew Wolcott <matt.wolc@gmail.com>
2025-10-16 20:07:30 -0700

    Set up CMake and a hello world file

---
+++ Pad Enclosure/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// Copy to pi:
+// scp -r "C:\Users\matt\Documents\GitHub\GSE-Embedded-Software\Pad Enclosure\."  matt@10.0.0.210:/home/matt/Desktop/Pad-Enclosure/
+// Compile on Pi:
+// cmake ..; make
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
